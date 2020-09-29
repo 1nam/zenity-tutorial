7043ebcf24be5c4b28240fb4355701c5e48c108c
zeus <33592162+1nam@users.noreply.github.com>
2020-09-29 09:40:17 -0400

    Update fake_error.sh

--- fake_error.sh
+++ fake_error.sh
@@ -5,18 +5,18 @@
 # or bash else Loop an error message using command yes.
 # note: bash is case sensitive use lower case.
 
-# bash debug starts
+# bash starts    Note: gets users input if user types debug zenity Gui if not Loops.
 read -p "For debug-info please type debug: " raw
 if [ $raw == "debug" ]
 then
 #bash ends
 
-# zenity GUI starts
+# zenity GUI starts   Note: this only happens id the user types debug.
 zenity --error \
 --text="/var/log/syslog Debug-info no errors!"
 # zenity ends
 
-# bash Loop starts
+# bash Loop starts   Note: if the user does not type debug this happens.
 else
 yes '''[Debug-error/var/log/syslog not found!] "CTRL + C = STOP!'''
 fi
