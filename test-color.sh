d518f98c2aa64d672c52adea2f338344148219d4
zeus <33592162+1nam@users.noreply.github.com>
2020-09-29 17:44:58 -0400

    Create test-color.sh

---
+++ test-color.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# 1nam testing bash and zenityl
+
+# if yes you choose a color echos the color code.
+# elif no strings good bye...
+# if yes or no is not typed you get an error.
+
+read -p "would you like to choose a color? " raw
+if [ $raw == "yes" ]
+then
+
+# if yes zenity starts the GUI
+
+COLOR=`zenity --color-selection --show-palette`
+
+case $? in
+         0)
+		echo "You selected $COLOR.";;
+         1)
+                echo "No color selected.";;
+        -1)
+                echo "An unexpected error has occurred.";;
+esac
+
+# zenity ends.
+
+elif [ $raw == "no" ]
+then
+echo "Good Bye..."
+fi
